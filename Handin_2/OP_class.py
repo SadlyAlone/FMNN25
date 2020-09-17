de05e95b85f07263f3725ce0d195433de1be87a0
William Winberg <william.winberg@gmx.com>
2020-09-17 13:53:15 +0200

    Added parent class for optimization methods

--- Handin_2/OP_class.py
+++ Handin_2/OP_class.py
@@ -1,7 +1,6 @@
 class OP_class:
-    def __init(self, function = lambda x: 0, gradient):
+    def __init(self, function, gradient=None):
         self.function = function
         self.gradient = gradient
     def __call__(x)
         return self.function(x)
-        
--- Handin_2/OP_class/OP_class.py
+++
@@ -1,7 +0,0 @@
-class OP_class:
-    def __init(self, function = lambda x: 0, gradient):
-        self.function = function
-        self.gradient = gradient
-    def __call__(x)
-        return self.function(x)
-        
