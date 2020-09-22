d2525fdf0ce6decb7820dc3f9b4aadbaca76b7ea
Picidae <kirtov.cw@gmail.com>
2020-09-22 13:41:02 +0200

    [Notation] Minor changes and patches
    Set file to import all classes, fixed simple test function and added Rosenbrock function

--- Handin_2/main.py
+++ Handin_2/main.py
@@ -1,5 +1,7 @@
-from classes.OP_class import OP_class
-from classes.opt_method_class import opt_method_class
-f = lambda x,y: x^2 + y^2
-op = OP_class(f)
+from classes import *
+
+f = lambda x,y: x**2 + y**2
+f_rosenbrock = lambda x,y: 100*(y - x**2)**2 + (1 - x)**2
+
+op = optimisation_problem_class(f)
 regular_newton = regular_newton(op)
